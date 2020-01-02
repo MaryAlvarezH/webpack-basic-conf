6d3c4bda6b72eb315b7573c892173f29daa41aed
MaryAlvarezH <maria.alvarez9409@gmail.com>
2020-01-02 11:03:36 -0600

    add css build webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 const path = require("path");
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
-  //   entry: "./src",
-  //   output: {
-  //     path: path.resolve(__dirname, "dist"),
-  //     filename: "bundle.js"
-  //   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                // publicPath is the relative path of the resource to the context
+                // e.g. for ./css/admin/main.css the publicPath will be ../../
+                // while for ./css/main.css the publicPath will be ../
+                return path.relative(path.dirname(resourcePath), context) + "/";
+              }
+            }
+          },
+          "css-loader"
+        ]
       }
     ]
   }
